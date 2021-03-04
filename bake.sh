c5af2b78d81397f0cf4f0eea8d2c8a36943bdb4d
Tyler Walker <tylerw.walker@hunterstrategy.net>
2021-03-04 13:14:03 -0800

    fixed repo name ref

--- bake.sh
+++ bake.sh
@@ -7,14 +7,14 @@ function dockerCheck () {
     echo 'THIS SCRIPT WILL REPLACE BUT BACKUP YOUR EXISTING KUBERNETES CONTEXT'
     echo "IF YOUR CONTEXT EXISTED BEFORE, IT IS NOW AT ~/.kube/config.backup.$now"
     cp ~/.kube/config ~/.kube/config.backup.$now
-#    while true; do
-#    read -p "Do you wish to run this demo given the previous conditions? y/n: > " yn
-#      case $yn in
-#          [Yy]* ) echo 'Running demo'; sleep 1; break;;
-#          [Nn]* ) echo 'Exiting'; exit;;
-#          * ) echo "Please answer yes or no.";;
-#      esac
-#    done
+    while true; do
+    read -p "Do you wish to run this demo given the previous conditions? y/n: > " yn
+      case $yn in
+          [Yy]* ) echo 'Running demo'; sleep 1; break;;
+          [Nn]* ) echo 'Exiting'; exit;;
+          * ) echo "Please answer yes or no.";;
+      esac
+    done
   else
     echo "Please install and or start Docker run this demo"
     exit
@@ -22,11 +22,11 @@ function dockerCheck () {
 }
 
 checkExist () {
-  if ${PWD##*/} == 'puppet-code-challenge'; then
+  if "$(basename $(pwd))" == "pup-mini"; then
     echo 'Code challenge already exists locally'
   else
-    docker run -ti --rm -v ${HOME}:/root -v $(pwd):/git alpine/git https://github.com/brokorus/puppet-code-sample.git
-    cd puppet-code-sample
+    docker run -ti --rm -v ${HOME}:/root -v $(pwd):/git alpine/git clone git@github.com:brokorus/pup-mini.git
+    cd pup-mini
   fi
   export parentDir=$(pwd)
 }
